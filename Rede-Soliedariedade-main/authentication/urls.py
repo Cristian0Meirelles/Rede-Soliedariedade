b28d85453d1cc906dd51f18513e5451a4e38c7f4
Cristian0Meirelles <59873755+Cristian0Meirelles@users.noreply.github.com>
2022-12-22 20:43:46 -0300

    Delete Rede-Soliedariedade-main directory

--- Rede-Soliedariedade-main/authentication/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from authentication import views
-
-urlpatterns = [
-    path('entrar', views.login_action, name='login'),
-    path('sair', views.logout_action, name='logout'),
-]
--- Rede-Soliedariedade-main/gestao_de_doacoes/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class GestaoDeDoacoesConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'gestao_de_doacoes'
-    verbose_name = 'Gestão de doações'
